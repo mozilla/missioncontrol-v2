1c155f0ffc0758bd4236757f4c51e6828a2f029a
Saptarshi Guha <saptarshi.guha@gmail.com>
2019-10-30 17:03:12 +0000

    redirected log messages

--- complete.runner.sh
+++ complete.runner.sh
@@ -2,6 +2,7 @@
 
 export PYTHONNOUSERSITE=True
 conda activate mc2
-Rscript download.data.and.build.model.R
-Rscript process.model.and.build.board.R
-Rscript create.dashboards.static.R
+Rscript download.data.and.build.model.R > logfile 2>&1
+Rscript process.model.and.build.board.R >> logfile 2>&1
+Rscript create.dashboards.static.R >> logfile 2>&1
+

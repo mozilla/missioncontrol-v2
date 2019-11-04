a12a52ac3b8b48a726830710eee9faafc3542d94
Saptarshi Guha <saptarshi.guha@gmail.com>
2019-11-04 22:23:07 +0000

    updated the shell script to load conda

--- complete.runner.sh
+++ complete.runner.sh
@@ -1,5 +1,20 @@
-#!/bin/sh
+#!/bin/bash
 ## This should be run in this folder (i.e. missioncontrol-v2)
+
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/home/sguha/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/home/sguha/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/home/sguha/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/home/sguha/anaconda3/bin:$PATH"
+    fi
+fi
+unset __conda_setup
+# <<< conda initialize <<<
 
 export PYTHONNOUSERSITE=True
 ## see https://github.com/conda/conda/issues/7980 for activating conda in a bash script

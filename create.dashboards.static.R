5bd510f2b06b421d9a0bb23e6318393f0422cf7e
Saptarshi Guha <saptarshi.guha@gmail.com>
2019-11-04 20:38:58 +0000

    rsync options fixed

--- create.dashboards.static.R
+++ create.dashboards.static.R
@@ -15,7 +15,7 @@ genf <- function(D,ch=c("release beta nightly faq")){
         if(grepl("nightly",ch)) render("mc2/nightly.Rmd",params=list(dest=D))
         if(grepl("faq",ch)) render("mc2/faq.Rmd",params=list(dest=D))        
         system("rsync  --exclude '*py' --exclude '*cache' --exclude 'data' --exclude 'tests'  -avz mc2/ ~/html/public/")
-        system(glue("gsutil -m rsync -d  ~/html/public/  gs://moz-fx-data-derived-datasets-analysis/sguha/missioncontrol-v2/html/public/"))
+        system(glue("gsutil -m rsync -d -r  ~/html/public/  gs://moz-fx-data-derived-datasets-analysis/sguha/missioncontrol-v2/html/public/"))
     }else if(D=="moco"){
         if(grepl("release",ch)) render("mc2/release.Rmd",params=list(dest=D));
         if(grepl("beta",ch)) render("mc2/beta.Rmd",params=list(dest=D));
@@ -24,8 +24,8 @@ genf <- function(D,ch=c("release beta nightly faq")){
         system("rsync  --exclude '*py' --exclude '*cache' --exclude 'data' --exclude 'tests'  -avz mc2/ ~/html/private/")
         system(glue("mkdir ~/html//archive/{loc}; rsync -avz ~/html/private/  ~/html/archive/{loc}/",
                     loc=dall.rel2[,max(date)]))
-        system(glue("gsutil -m rsync -d  ~/html/private/  gs://moz-fx-data-derived-datasets-analysis/sguha/missioncontrol-v2/html/private/"))
-        system(glue("gsutil -m rsync -d  ~/html/archive/{loc}/  gs://moz-fx-data-derived-datasets-analysis/sguha/missioncontrol-v2/html/archive/{loc}/"
+        system(glue("gsutil -m rsync -d -r  ~/html/private/  gs://moz-fx-data-derived-datasets-analysis/sguha/missioncontrol-v2/html/private/"))
+        system(glue("gsutil -m rsync -d  -r ~/html/archive/{loc}/  gs://moz-fx-data-derived-datasets-analysis/sguha/missioncontrol-v2/html/archive/{loc}/"
                   , loc=dall.rel2[,max(date)]))
     }
 }

fb73f250fc37461353642ad60918924b7c95156a
Saptarshi Guha <saptarshi.guha@gmail.com>
2019-11-05 07:46:49 -0800

    script edits

--- complete.runner.R
+++ complete.runner.R
@@ -1,4 +1,4 @@
 source("download.data.and.build.model.R")
 source("process.model.output.and.save.R")
 source("create.dashboards.static.R")
-
+quit(status=0)
--- local.run.on.gcp.sh
+++ local.run.on.gcp.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-gcloud beta compute instances --project "moz-fx-dev-sguha-rwasm" start  "instance-1"   
+gcloud beta compute instances --project "moz-fx-dev-sguha-rwasm" start  "instance-1"
+sleep 10
 gcloud beta compute  --project "moz-fx-dev-sguha-rwasm" ssh  "instance-1" --command " cd /home/sguha/missioncontrol-v2 ; rm -rf logfile; sh complete.runner.sh  2>&1 | tee logfile"
-gsutil -m rsync -d -r gs://moz-fx-data-derived-datasets-analysis/sguha/missioncontrol-v2/html/private/ /tmp/private/ && rsync -avz /tmp/private ~/mz/missioncontrol/ex1/mc2/
-gsutil -m rsync -d -r gs://moz-fx-data-derived-datasets-analysis/sguha/missioncontrol-v2/html/public/ /tmp/public/ && rsync -avz /tmp/public ~/pubsguha/mc2/
+rc=$?;
+if [[ $rc != 0 ]];  then 
+           gsutil -m rsync -d -r gs://moz-fx-data-derived-datasets-analysis/sguha/missioncontrol-v2/html/private/ /tmp/private/ && rsync -avz /tmp/private ~/mz/missioncontrol/ex1/mc2/
+           gsutil -m rsync -d -r gs://moz-fx-data-derived-datasets-analysis/sguha/missioncontrol-v2/html/public/ /tmp/public/ && rsync -avz /tmp/public ~/pubsguha/mc2/
+else
+    echo "ERROR ERROR"
+fi   
 gcloud beta compute instances --project "moz-fx-dev-sguha-rwasm" stop  "instance-1"   

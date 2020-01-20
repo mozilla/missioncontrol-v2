687b958037d8c1ed9392750c595696d10b54e96e
Saptarshi Guha <saptarshi.guha@gmail.com>
2020-01-20 01:15:20 +0000

    added back file

--- missioncontrol.lib.R
+++ missioncontrol.lib.R
@@ -152,7 +152,8 @@ make.a.model <- function(data,wh,channel='not-nightly',debug=0,bff=NULL,list0=NU
             }
         }
         if(channel %in% c("nightly")){
-            M0 <- bf( cmain + 1  ~ offset(log(usage_cm_crasher_cversion + 1/60)) + os + (1+os | c_version) +  log(nvc)*osc)+negbinomial()
+            M0 <- bf( cmain + 1  ~ offset(log(usage_cm_crasher_cversion + 1/60)) + os + (1+os | c_version) +  log(nvc)*os,
+                     shape ~ os)+negbinomial()
             if(debug==1){
              M0 <- bf( cmain + 1  ~ offset(log(usage_cm_crasher_cversion + 1/60)) + os *log(nvc))+negbinomial()
             }
